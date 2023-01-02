5170aaadc6e11dab391076fca37e06d60c0dc07f
vituhonorato <vituhonorato.89@gmail.com>
2023-01-02 01:41:45 -0300

    add GUI

---
+++ src/main/java/com/example/Gui.java
@@ -0,0 +1,19 @@
+
+package main.java.com.example;
+import javax.swing.JOptionPane;
+
+public class Gui {
+
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello"+name);
+        
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are "+ age + "years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter our height"));    
+        JOptionPane.showMessageDialog(null, "Your height is " + height);
+
+
+ }
+}
