a79fa639dd47f2864cb110a3fe27869b37854bae
vituhonorato <vituhonorato.89@gmail.com>
2023-01-03 16:34:28 -0300

    Add ForLoops

---
+++ src/main/java/com/example/demo/ForLoops.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class ForLoops {
+    public static void main (String[] args) {
+
+    //     for(int i = 0; i<=10 ;i++){
+    //         System.out.println(i);
+    //     }
+    //    System.out.println("Happy new year");
+
+       for(int i = 10; i>=0 ;i--){
+        System.out.println(i);
+    }
+   System.out.println("Happy new year");
+    }; 
+
+
+}
