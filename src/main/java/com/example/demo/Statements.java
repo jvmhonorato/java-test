982d1ad62c6cf6513e61af71abfb0d6764a77c09
vituhonorato <vituhonorato.89@gmail.com>
2023-01-03 10:30:33 -0300

    add Steatments file to learn conditionals if else

---
+++ src/main/java/com/example/demo/Statements.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class Statements {
+    public static void main (String[] args) {
+      
+        int age = 13;
+        if(age>=18){
+            System.out.println("you are adult");
+        }else if(age<18 && age>12){
+            System.out.println("You are teenage");
+        }else{
+            System.out.println("You are a child");
+        }
+
+
+    };
+}
