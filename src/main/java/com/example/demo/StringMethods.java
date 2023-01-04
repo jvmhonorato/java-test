b246e1a5630fc1eb47b703329c191bdad65af7a4
vituhonorato <vituhonorato.89@gmail.com>
2023-01-04 10:25:02 -0300

    add String methods

---
+++ src/main/java/com/example/demo/StringMethods.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class StringMethods {
+    public static void main (String[] args) {
+
+        String name = "Victor";
+    //    boolean result = name.equalsIgnoreCase("Victor");
+    //    int result = name.length();
+    //    char result = name.charAt(0);
+    //    int result = name.indexOf("o");
+    //    boolean result = name.isEmpty();
+    //    String result = name.toUpperCase();
+    //    String result = name.toLowerCase();
+    //    String result = name.trim();
+       String result = name.replace('o', 'a');
+
+       
+
+        System.out.println(result);
+
+    };
+}
