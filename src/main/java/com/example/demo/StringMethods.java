2afc7715fe8bdcea5e7dbb9efb3efbd41c8835f5
vituhonorato <vituhonorato.89@gmail.com>
2023-01-05 23:54:18 -0300

    declarete t string methods

--- src/main/java/com/example/demo/StringMethods.java
+++ src/main/java/com/example/demo/StringMethods.java
@@ -6,11 +6,23 @@ public class StringMethods {
         String name = "Victor";
     //    boolean result = name.equalsIgnoreCase("Victor");
     //    int result = name.length();
+
+     // 
     //    char result = name.charAt(0);
+
+    // search for declareted caracterer
     //    int result = name.indexOf("o");
+      
+      //is empty? true  or false
     //    boolean result = name.isEmpty();
+
+      //turn text lowercase
     //    String result = name.toUpperCase();
+
+       //turn texct uppercase
     //    String result = name.toLowerCase();
+
+      //remove spaces between
     //    String result = name.trim();
        String result = name.replace('o', 'a');
 
