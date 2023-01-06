e53e5aa8272c7473bb205a8d3d31be4111a2cf59
vituhonorato <vituhonorato.89@gmail.com>
2023-01-06 00:21:08 -0300

    add Array list add, set , remove and clear

--- src/main/java/com/example/demo/ArrayList2d.java
+++ src/main/java/com/example/demo/ArrayList2d.java
@@ -1,5 +1,28 @@
 package com.example.demo;
+import java.util.ArrayList;
 
 public class ArrayList2d {
-    public static void main (String[] args) {};
+    public static void main (String[] args) {
+
+        ArrayList<String> food = new ArrayList<String>();
+         
+        food.add("pizza");
+        food.add("hamburguer");
+        food.add("hotdog");
+        
+        //set method replace the declared array key
+        food.set(0, "sushi");
+
+        //remove object declared by array key
+        food.remove(2);
+
+        //remove all array elements
+        food.clear();
+
+        for(int i=0 ; i<food.size(); i++) {
+            System.out.println(food.get(i));
+        }
+
+
+    };
 }
