b4046a65a1e0bf316279c8c87944f3c313742623
vituhonorato <vituhonorato.89@gmail.com>
2023-01-31 18:48:31 -0300

    add FinalKeyword

---
+++ src/main/java/com/example/demo/FinalKeyword.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class FinalKeyword {
+    public static void main (String[] args) {
+
+        //use final to let variable immutable
+       final double PI = 3.14159;
+
+       
+    System.out.println(PI);
+    
+    }
+    
+}
